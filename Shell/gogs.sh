62fe7e2c69ea406369106c727ce37e39f5cc8a1d
GitXpresso <gitxpresso@outlook.com>
2024-11-18 01:57:59 +0000

    Add new bash file

--- Shell/gogs.sh
+++ Shell/gogs.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+sudo adduser --disabled-login --gecos 'Gogs' git
+# Clone the repository to the "gogs" subdirectory
+git clone --depth 1 https://github.com/gogs/gogs.git gogs
+# Change working directory
+cd gogs
+# Compile the main program, dependencies will be downloaded at this step
+go build -o gogs
