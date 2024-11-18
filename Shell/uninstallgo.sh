b0928898a8774c9fa66521e773db6bf323201a08
GitXpresso <gitxpresso@outlook.com>
2024-11-18 00:38:43 +0000

    Add uninstallgo.sh bash file

---
+++ Shell/uninstallgo.sh
@@ -0,0 +1,27 @@
+#!/bash/bin
+# Command that find where go is located
+which go
+# Removing go
+sudo rm -rf /usr/local/go
+# Downloading Go
+wget https://golang.org/dl/go1.22.1.linux-amd64.tar.gz
+# Extracting Go
+sudo tar -C -xvf go1.22.1.linux-amd64.tar.gz
+# Moving Go to /usr/local
+sudo mv go /usr/local
+# Adding Go to PATH
+export PATH=$PATH:/usr/local/go/bin
+# Checking Go version
+go version
+# Removing Go download file
+rm go1.22.1.linux-amd64.tar.gz
+# Adding Go to PATH permantently
+echo "export PATH=$PATH/usr/local/go/bin" >> ~\.bashrc
+# Reloading bash 
+source ~\.bashrc
+# Checking Go version
+go version
+# Checking Go location
+which go
+# Checking Go environment variables
+go env
