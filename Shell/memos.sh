54f1e6f7548d705383bd317e39b030429f615ce3
GitXpresso <gitxpresso@outlook.com>
2024-11-18 18:35:13 +0000

    memos.sh

--- Shell/memos.sh
+++ Shell/memos.sh
@@ -2,13 +2,10 @@
 
 # this commands install local tunnel using npm
 npm install -g localtunnel
-# this command checks that local tunnel successfully installed
-lt
 # this command checks your local tunnelpassword
 curl https://loca.lt/mytunnelpassword
 echo "copy and paste the tunnelpassword aka your ip address in the loca.lt url"
 # installs and run memos
 docker run -d --name memos -p 5230:5230 -v ~/.memos/:/var/opt/memos neosmemo/memos:stable
 # this sets a url for the running docker container port
-lt --port 5230
 
