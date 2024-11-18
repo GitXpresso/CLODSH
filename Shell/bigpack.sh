4ac055050fceef087e11afd860cf84a2d33c8e8a
GitXpresso <gitxpresso@outlook.com>
2024-11-18 19:15:14 +0000

    add immich app and updated bigpack.sh

--- Shell/bigpack.sh
+++ Shell/bigpack.sh
@@ -6,6 +6,8 @@ chmod u+x gh.sh
 # this makes dockerkali.sh executable 
 chmod u+x dockerkali.sh
 # this runs and installs dockerkali.sh
-./dockerkali.sh
+bash ./dockerkali.sh
 # this make executable composekali.sh
 chmod u+x composekali.sh
+# this installs compose
+bash ./composekali.sh
