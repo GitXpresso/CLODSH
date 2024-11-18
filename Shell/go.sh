a94182de03f4901ddd8d4ea77fceb481814b8917
William <126926699+GitXpresso@users.noreply.github.com>
2024-11-17 19:41:39 -0500

    Update go.sh
    Add bash script to go.sh

---
+++ Shell/go.sh
@@ -0,0 +1,23 @@
+#!/bash/bin
+# Downloading Go
+wget https://golang.org/dl/go1.22.1.linux-amd64.tar.gz
+# Extracting Go
+sudo tar -C -xvf go1.22.1.linux-amd64.tar.gz
+# Moving Go to /usr/local
+sudo mv go /usr/local
+# Adding Go to PATH
+export PATH=$PATH:/usr/local/go/bin
+# Checking Go version
+go version
+# Removing Go download file
+rm go1.22.1.linux-amd64.tar.gz
+# Adding Go to PATH permantently
+echo "export PATH=$PATH/usr/local/go/bin" >> ~\.bashrc
+# Reloading bash 
+source ~\.bashrc
+# Checking Go version
+go version
+# Checking Go location
+which go
+# Checking Go environment variables
+go env
