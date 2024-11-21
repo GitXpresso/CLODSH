d54838db647b7090bf9aa6998361d05048443f68
GitXpresso <gitxpresso@outlook.com>
2024-11-21 17:58:13 +0000

    updated two 2 sh fils and moved y.sh from Coder directory to Shell/Tests directory

--- Coder/y.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-echo yes y | sudo apt update
-if ! which docker > /dev/null; then
-   echo -e "Command not found! Install? (y/n) \c"
-
---
+++ Shell/Tests/y.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+BIWhite='\033[1;97m'      # White
+echo -e "${BIWhite}test"
