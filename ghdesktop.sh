2c7ca22fd28b421e07ebb2aa8d044e2e1146ee31
William <126926699+GitXpresso@users.noreply.github.com>
2024-12-17 16:03:39 -0500

    Create ghdesktop.sh

---
+++ ghdesktop.sh
@@ -0,0 +1,7 @@
+
+wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list'
+updating packages
+sudo apt-get update
+sudo apt install github-desktop
+echo -e "Hooray! Github Desktop is installed."
