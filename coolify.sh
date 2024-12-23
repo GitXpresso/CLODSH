bbe087d2533959984576bd34a83e9fe75dd9c573
William <126926699+GitXpresso@users.noreply.github.com>
2024-12-23 15:18:14 -0500

    Update coolify.sh
    Add contents part 1

--- coolify.sh
+++ coolify.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+echo "Installing coolify the manual way"
+mkdir -p /data/coolify/{source,ssh,applications,databases,backups,services,proxy,webhooks-during-maintenance}
+mkdir -p /data/coolify/ssh/{keys,mux}
+mkdir -p /data/coolify/proxy/dynami
 
+ssh-keygen -f /data/coolify/ssh/keys/id.root@host.docker.internal -t ed25519 -N '' -C root@coolify
