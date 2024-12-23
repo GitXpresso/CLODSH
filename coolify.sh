b4743f938177f11fa4e8359b0884abecdd2480ea
William <126926699+GitXpresso@users.noreply.github.com>
2024-12-23 15:21:00 -0500

    Update coolify.sh

--- coolify.sh
+++ coolify.sh
@@ -15,3 +15,11 @@ curl -fsSL https://cdn.coollabs.io/coolify/upgrade.sh -o /data/coolify/source/up
 
 chown -R 9999:root /data/coolify
 chmod -R 700 /data/coolify
+
+sed -i "s|APP_ID=.*|APP_ID=$(openssl rand -hex 16)|g" /data/coolify/source/.env
+sed -i "s|APP_KEY=.*|APP_KEY=base64:$(openssl rand -base64 32)|g" /data/coolify/source/.env
+sed -i "s|DB_PASSWORD=.*|DB_PASSWORD=$(openssl rand -base64 32)|g" /data/coolify/source/.env
+sed -i "s|REDIS_PASSWORD=.*|REDIS_PASSWORD=$(openssl rand -base64 32)|g" /data/coolify/source/.env
+sed -i "s|PUSHER_APP_ID=.*|PUSHER_APP_ID=$(openssl rand -hex 32)|g" /data/coolify/source/.env
+sed -i "s|PUSHER_APP_KEY=.*|PUSHER_APP_KEY=$(openssl rand -hex 32)|g" /data/coolify/source/.env
+sed -i "s|PUSHER_APP_SECRET=.*|PUSHER_APP_SECRET=$(openssl rand -hex 32)|g" /data/coolify/source/.env
