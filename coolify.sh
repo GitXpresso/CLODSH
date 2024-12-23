4247a7d30587ee180112ade12e59edf0aa92ce51
William <126926699+GitXpresso@users.noreply.github.com>
2024-12-23 15:19:15 -0500

    Update coolify.sh

--- coolify.sh
+++ coolify.sh
@@ -5,3 +5,9 @@ mkdir -p /data/coolify/ssh/{keys,mux}
 mkdir -p /data/coolify/proxy/dynami
 
 ssh-keygen -f /data/coolify/ssh/keys/id.root@host.docker.internal -t ed25519 -N '' -C root@coolify
+
+
+curl -fsSL https://cdn.coollabs.io/coolify/docker-compose.yml -o /data/coolify/source/docker-compose.yml
+curl -fsSL https://cdn.coollabs.io/coolify/docker-compose.prod.yml -o /data/coolify/source/docker-compose.prod.yml
+curl -fsSL https://cdn.coollabs.io/coolify/.env.production -o /data/coolify/source/.env
+curl -fsSL https://cdn.coollabs.io/coolify/upgrade.sh -o /data/coolify/source/upgrade.sh
