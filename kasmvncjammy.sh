42ecc5113a20fa5b90ce1a199ade049b7daaf98b
William <126926699+GitXpresso@users.noreply.github.com>
2024-12-24 17:59:04 -0500

    Update kasmvncjammy.sh

--- kasmvncjammy.sh
+++ kasmvncjammy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "grabbing the kasmvnc jammy debian file"
-wget -O kasmvnc.deb https://github.com/kasmtech/KasmVNC/releases/download/v1.3.3/kasmvncserver_jammy_1.3.3_amd64.deb 
+wget -O kasmvnc.deb https://github.com/kasmtech/KasmVNC/releases/download/v1.3.3/kasmvncserver_jammy_1.3.3_amd64.deb
+echo "Updating Packages in order to successfully install kasmvnc"
+sudo apt update
 echo "Installing the newly added debian file"
 sudo apt install -y ./kasmvnc.deb
+echo "Kasmvnc successfully installed"
+echo "put this "kasmvncserver" in your terminal to setup kasmvnc"
+echo "here is a link to help you with packsges that came with kasmvnc debian file, https://www.kasmweb.com/kasmvnc/docs/latest/serverside.html"
