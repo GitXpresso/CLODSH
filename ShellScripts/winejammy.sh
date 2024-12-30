c8867c916589da6f3298cff84c6f934b7a13bced
William <126926699+GitXpresso@users.noreply.github.com>
2024-12-30 10:48:03 -0500

    Create winejammy.sh

---
+++ ShellScripts/winejammy.sh
@@ -0,0 +1,10 @@
+
+echo -e "enabling 32bit support for ubuntu"
+sudo dpkg --add-architecture i386 
+echo "Adding the Wine Repo Keys"
+echo "creating keyring file"
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+sudo apt update && sudo apt install --install-recommends winehq-stable
+sudo winecfg
